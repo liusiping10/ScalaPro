aa136f95239a33a8d178e385e54cead3bbdcc3cd
刘四平 <liusiping30@qq.com>
2015-03-14 09:56:07 +0800

    test

--- src/Test.scala
+++ src/Test.scala
@@ -4,5 +4,19 @@
 object Test {
   def main(args: Array[String]) {
     println("this is a test")
+
+    val list=Range(1,10)
+    list.foreach(println)
+
+    val list2=Range(1,10,2)
+    list2.foreach(println)
+
+
   }
 }
+
+//object Hello extends App{
+//  println("hello this a no main method object")
+//}
+
+
